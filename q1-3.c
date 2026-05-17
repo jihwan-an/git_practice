5805e957938f38482916ef628daaf2aefca345cf
heyhoheyhoking <heyhoheyhoking@gmail.com>
2026-05-17 14:08:19 +0900

    9th commit

--- q1-3.c
+++ q1-3.c
@@ -1,7 +1,20 @@
 include <stdio.h>
-
+int min(int na, int nb)
+{
+	if (na<nb)
+		return na;
+	return nb;
 int main(void)
 {
-	printf("wrong text);
+	int a, b;
+	printf("두 개의 정수 입력");
+	scanf("%d %d", &a, &b);
+	
+	int i;
+
+	for (i=min(a,b); i>=1; i--)
+		if (a%i == 0 && b%i ==0)
+			break;
+	printf("두 수의 최대공약수: %d", i);
 	return 0;
 }
---
+++ q
@@ -0,0 +1,7 @@
+include <stdio.h>
+
+int main(void)
+{
+	printf("wrong text);
+	return 0;
+}
