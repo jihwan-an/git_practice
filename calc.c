7ea13873ef01f17ee607b626086a9a146430f5f2
heyhoheyhoking <heyhoheyhoking@gmail.com>
2026-05-31 14:02:39 +0900

    add code in calc

--- calc.c
+++ calc.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main(void)
+{
+	printf("Set mode [plus=0/minus=1]");
+	int a;
+	scanf("%d", &a);
+	
+	printf("two number");
+	int b,c;
+	scanf("%b %c", &a, &b);
+	if (a==0)
+		printf("%d \n", b+c);
+	else
+		printf("%d \n", b-c);
+	return 0;
+}
